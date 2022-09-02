6395a741a8f048ae5b9ab8a5ae4172dc29bb6773
Jonas Humberto <92595788+Jonas1berto@users.noreply.github.com>
2022-09-02 12:30:40 -0400

    add button to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@ const txtLevel = document.getElementById('txtLevel')
 const txtCharacter = document.getElementById('txtCharacter')
 const btnSubscribe = document.getElementById('btnSubscribe')
 
+const btnTop = document.querySelector('#backToTop')
+
 
 btnSubscribe.addEventListener('click', async () => {
   const subscription = {
@@ -23,4 +25,23 @@ btnSubscribe.addEventListener('click', async () => {
   txtCharacter.value = ''
 
   alert('Inscrito com sucesso')
+})
+
+// quando rolar a página 100px do topo para baixo, o botão aparece
+
+window.onscroll = function () {
+  scrollFunction()
+}
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btnTop.style.display = 'block'
+  } else {
+    btnTop.style.display = 'none'
+  }
+}
+
+// quando clicar no botão, a pagina voltará para o inicio
+
+btnTop.addEventListener('click', function () {
+  window.scrollTo(0, 0)
 })
